5d08b8af63b3a2d5da0b8a26e46a81b290b87528
WSchrader <wilrader@gmail.com>
2014-02-28 12:13:54 -0800

    duuurp

--- wafflesirc/wafflesirc.js
+++ wafflesirc/wafflesirc.js
@@ -3,7 +3,7 @@ var irc = require('irc');
 var options = {
 	    userName: 'wafflesbrobot',
 	    realName: 'nodeJS IRC client',
-	    port: 6667,
+	    port: 8002,
 	    debug: true,
 	    showErrors: true,
 	    autoRejoin: true,
@@ -16,9 +16,7 @@ var options = {
 	    floodProtectionDelay: 1000,
 	}
 
-var client = new irc.Client('irc.freenode.net', 'wafflesbrobot', {
-    channels: ['#wafflestest'],
-});
+var client = new irc.Client('irc.freenode.net', 'wafflesbrobot', options);
 
 client.addListener('message', function (from, to, message) {
     console.log(from + ' => ' + to + ': ' + message);
